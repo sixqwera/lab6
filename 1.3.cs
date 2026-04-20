ce8cbea8835a41d73fa62f3a17754c79066b1932
sixqwera <nikcharushnikov@gmail.com>
2026-04-21 02:29:36 +0300

    1.3.cs

--- 1.3.cs
+++ 1.3.cs
@@ -1,87 +1,4 @@
 using System;
-using CSharpLab;
-
-internal class Program
-{
-    private static void Main(string[] args)
-    {
-        int ReadInt(string message)
-        {
-            int result;
-            while (true)
-            {
-                Console.Write(message);
-                if (int.TryParse(Console.ReadLine(), out result))
-                {
-                    return result;
-                }
-                Console.WriteLine("Ошибка! Введите целое число.");
-            }
-        }
-
-        Console.WriteLine("=== Данные для Lab4 ===");
-        int f1 = ReadInt("Число 1: ");
-        int s1 = ReadInt("Число 2: ");
-        int t1 = ReadInt("Число 3: ");
-
-        var obj1 = new Lab4(f1, s1, t1);
-        Console.WriteLine($"Объект создан: {obj1}");
-        obj1.LastMaxValue();
-
-        Console.WriteLine("\n--- Проверка копирования ---");
-        var copyObj = new Lab4(obj1);
-        Console.WriteLine($"Данные копии: {copyObj}");
-
-        Console.WriteLine("\n=== Данные для NewLab ===");
-        int nf1 = ReadInt("Число 1: ");
-        int ns1 = ReadInt("Число 2: ");
-        int nt1 = ReadInt("Число 3: ");
-        int nfo = ReadInt("Число 4: ");
-
-        var childObj = new NewLab(nf1, ns1, nt1, nfo);
-
-        childObj.Sum(nf1, ns1, nt1);
-        childObj.Mul(nf1, ns1, nt1);
-
-        Console.WriteLine("\nНажмите любую клавишу для выхода...");
-        Console.ReadKey();
-    }
-}
-метод внутри метода низя 
-
-
-
-
-using System;
-
-namespace CSharpLab
-{
-    public class NewLab : Lab4
-    {
-        private int _fourth;
-
-        public NewLab(int f, int s, int t, int fo) : base(f, s, t)
-        {
-            _fourth = fo;
-        }
-
-        public void Sum(int f, int s, int t)
-        {
-            int sum = f + s + t + _fourth;
-            Console.WriteLine($"Сумма: {sum}");
-        }
-
-        public void Mul(int f, int s, int t)
-        {
-            int mul = f * s * t * _fourth;
-            Console.WriteLine($"Умножение: {mul}");
-        }
-    }
-}
-
-
-
-using System;
 
 namespace CSharpLab
 {
@@ -112,12 +29,13 @@ namespace CSharpLab
             int t1 = Math.Abs(_third % 10);
 
             int max = f1;
-            if (s1 > max) 
+            if (s1 > max)
             {
                 max = s1;
-            if (t1 > max) max = t1;
+                if (t1 > max) max = t1;
 
-            Console.WriteLine($"Максимальное число: {max}");
+                Console.WriteLine($"Максимальное число: {max}");
+            }
         }
 
         public override string ToString()
