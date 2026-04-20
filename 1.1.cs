ac0b449cb1ea0734abeedff7eeb5350aeeaa3ded
sixqwera <nikcharushnikov@gmail.com>
2026-04-21 02:56:54 +0300

    1.1.cs

--- 1.1.cs
+++ 1.1.cs
@@ -11,12 +11,12 @@ internal class Program
         int s1 = Checker.ReadInt("Число 2: ");
         int t1 = Checker.ReadInt("Число 3: ");
 
-        var obj1 = new Lab4(f1, s1, t1);
+        Lab4 obj1 = new Lab4(f1, s1, t1);
         Console.WriteLine($"Объект создан: {obj1}");
         obj1.LastMaxValue();
 
         Console.WriteLine("--- Проверка копирования ---");
-        var copyObj = new Lab4(obj1);
+        Lab4 copyObj = new Lab4(obj1);
         Console.WriteLine($"Данные копии: {copyObj}");
 
         Console.WriteLine("=== Данные для NewLab ===");
@@ -25,7 +25,7 @@ internal class Program
         int nt1 = Checker.ReadInt("Число 3: ");
         int nfo = Checker.ReadInt("Число 4: ");
 
-        var childObj = new NewLab(nf1, ns1, nt1, nfo);
+        NewLab childObj = new NewLab(nf1, ns1, nt1, nfo);
         childObj.Sum(nf1, ns1, nt1);
         childObj.Mul(nf1, ns1, nt1);
     }
