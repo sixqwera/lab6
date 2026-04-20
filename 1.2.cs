aaf42ec7e2ecf4a2b2a8c641f0af3889e3408809
sixqwera <nikcharushnikov@gmail.com>
2026-04-21 02:29:15 +0300

    1.2.cs

--- 1.2.cs
+++ 1.2.cs
@@ -1,71 +1,21 @@
 using System;
 
-namespace c__перывй_раз_
+namespace CSharpLab
 {
-    internal class Lab4
+    public static class Checker
     {
-        protected int first;
-        protected int second;
-        protected int third;
-        
-        public Lab4(int f, int s, int t)
+        public static int ReadInt(string message)
         {
-            first = f;
-            second = s;
-            third = t;
-        }
-
-        public Lab4(Lab4 lab4)
-        {
-            this.first = lab4.first;
-            this.second = lab4.second;
-            this.third = lab4.third;
-        }
-
-        public void LastMaxValue()
-        {
-            int first1 = first % 10;
-            int second1 = second % 10;
-            int third1 = third % 10;
-
-            int max = first1;
-
-            if (max < second1)
+            int result;
+            while (true)
             {
-                max = second1;
+                Console.Write(message);
+                if (int.TryParse(Console.ReadLine(), out result))
+                {
+                    return result;
+                }
+                Console.WriteLine("Ошибка! Введите целое число.");
             }
-            if (max < third1)
-            {
-                max = third1;
-            }
-            Console.WriteLine("Максимальное число: " + max);
-        }
-
-        public override string ToString()
-        {
-            return "First: " + first + ", Second: " + second + ", Third: " + third;
-        } 
-    }
-
-    class NewLab : Lab4
-    {
-        private int fourth;
-        public NewLab(int f, int s, int t, int fo) : base(f, s, t)
-        {
-            fourth = fo;
-        }
-
-        public void Sum()
-        {
-            int s = first + second + third + fourth;
-            Console.WriteLine("Суммма: " + s);
-        }
-
-        public void Mul()
-        {
-            int s = first * second * third * fourth;
-            Console.WriteLine("Умножение: " + s);
         }
     }
-
 }
