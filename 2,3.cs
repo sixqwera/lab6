e25036baed00534b57dd8918a8afd9f026215e17
sixqwera <nikcharushnikov@gmail.com>
2026-04-20 18:59:32 +0000

    Create 2,3.cs

---
+++ 2,3.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Checker
+    {
+        public static double CheckDouble(string prompt)
+        {
+            Console.Write(prompt);
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (double.TryParse(input, out double result))
+                {
+                    return result;
+                }
+
+                Console.Write("Ошибка! Введите число. " +
+                    "Повторите: ");
+            }
+        }
+
+        public static double CheckDouble(
+            string prompt,
+            double min,
+            double max)
+        {
+            Console.Write(prompt);
+            while (true)
+            {
+                string input = Console.ReadLine();
+                bool isDouble = double.TryParse(
+                    input,
+                    out double result);
+
+                if (isDouble && result >= min && result <= max)
+                {
+                    return result;
+                }
+
+                Console.Write($"Ошибка! Введите число " +
+                    $"от {min} до {max}: ");
+            }
+        }
+    }
+}
